8e74b57a50e705dde527d7c7de5d0dafbc110f9b
David Nguyen <davebox309@gmail.com>
2021-11-20 13:07:57 -0500

    finished addStudents

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,58 @@
 package io.zipcoder;
 
 public class Classroom {
+
+    private Student[] students;
+
+
+    public Classroom(int maxNumberOfStudents){
+
+        this.students = new Student[maxNumberOfStudents];
+    }
+
+    public Classroom(Student[] students){
+        this.students = students;
+    }
+
+    public Classroom(){
+
+        this.students = null;
+    }
+
+    public Student[] getStudents(){
+
+        return students;
+    }
+
+
+    public Double getAverageExamScore(){
+
+        Double sum = 0.0;
+
+        for(Student student : students){
+                sum += student.getAverageExamScore();
+            }
+
+
+        return sum / students.length;
+
+    }
+
+    public void addStudent(Student student){
+
+        for(int i =0; i < students.length; i++){
+            if(students[i] == null){
+                students[i] = student;
+                break;
+            }
+        }
+    }
+
+
+    public void removeStudent(String firstName, String lastName){
+
+        for
+
+    }
+
 }
--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,90 @@
 package io.zipcoder;
 
+import java.util.*;
+
 public class Student {
+
+
+    private String name, lastName;
+    private List<Double> examScores;
+
+
+    public Student(String firstName, String lastName, Double[] testScores){
+
+        this.name = firstName;
+        this.lastName= lastName;
+        this.examScores = Arrays.asList(testScores);
+
+    }
+
+    public int getNumberOfExamsTaken(){
+
+        return examScores.size();
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getExamScores() {
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < examScores.size(); i++){
+            sb.append("Exam " + (i + 1) + " -> " + examScores.get(i) + "\n");
+        }
+
+
+        return sb.toString();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void addExamScore(Double examScore){
+
+        examScores.add(examScore);
+
+    }
+
+    public void setExamScore(int examNumber, double newScore){
+
+        //setExamScore is trying to change the score in the list
+
+        examScores.set(examNumber-1, newScore);
+    }
+
+    public Double getAverageExamScore(){
+
+        Double sum = 0.0;
+
+        for(Double score : examScores){
+            sum += score;
+        }
+
+        return sum / examScores.size();
+    }
+
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Student Name: " + name  + " " + lastName + "\n");
+        sb.append("Average Score: " + getAverageExamScore() + "\n");
+        sb.append(getExamScores());
+
+        return sb.toString();
+    }
+
+
 }
